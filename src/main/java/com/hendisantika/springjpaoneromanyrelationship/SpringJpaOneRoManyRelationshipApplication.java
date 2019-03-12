a205a50c0cf8a3a088e7d8eebdc99f4dca78a251
hendisantika <hendisantika@yahoo.co.id>
2019-03-13 06:26:36 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springjpaoneromanyrelationship/SpringJpaOneRoManyRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springjpaoneromanyrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOneRoManyRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaOneRoManyRelationshipApplication.class, args);
+    }
+
+}
